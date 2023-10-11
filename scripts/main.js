fc2929d060180f9dfefc2ce4d51b2016d8db57a7
gabenicholson <gabenicholson@uchicago.edu>
2023-10-10 22:01:50 -0700

    feat: modal POC

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,20 @@
-const doSomething = () => {
-    
-}
-// function doSomething () {
-// }
 
-const person = {
-    age: 10,
-    name: "Gabe",
-    isMarried: false
+var modal = document.getElementById("myModal")
+var img = document.getElementById("image1")
+var span = document.getElementsByClassName("close")
+
+img.onclick = function() {
+    modal.style.display = "block"
 }
 
-console.log(person)
+// Close the modal when the 'x' is clicked
+span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+// Close the modal if the user clicks outside of it
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
